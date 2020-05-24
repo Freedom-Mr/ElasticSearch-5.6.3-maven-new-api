51fa5f40cb066cc9dc1fde4c54579ccc7da5e29f
Freedom-Mr <31268554+Freedom-Mr@users.noreply.github.com>
2020-05-24 13:46:33 +0800

    初始化

---
+++ src/main/java/casia/isiteam/api/elasticsearch/App.java
@@ -0,0 +1,12 @@
+package casia.isiteam.api.elasticsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
